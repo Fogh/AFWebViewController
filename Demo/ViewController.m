e0fc4f6dca93661cdd7ec995346d98266c523f5c
Anders Fogh Eriksen <anders@afogh.dk>
2014-11-15 12:05:51 +0100

    Changed to use class methods instead of init

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -13,12 +13,12 @@
 @implementation ViewController
 
 - (IBAction)openWebView:(id)sender {
-    AFWebViewController *webViewController = [[AFWebViewController alloc] initWithAddress:@"http://google.com"];
+    AFWebViewController *webViewController = [AFWebViewController webViewControllerWithAddress:@"https://google.com"];
     [self.navigationController pushViewController:webViewController animated:YES];
 }
 
 - (IBAction)openWebViewModal:(id)sender {
-    AFModalWebViewController *webViewController = [[AFModalWebViewController alloc] initWithAddress:@"http://google.com"];
+    AFModalWebViewController *webViewController = [AFModalWebViewController webViewControllerWithAddress:@"https://google.com"];
     [self presentViewController:webViewController animated:YES completion:NULL];
 }
 @end
