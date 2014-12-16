0b7bcaca33f91d78fccaa0ec7ade58514db27150
Anders Fogh Eriksen <anders@afogh.dk>
2014-12-16 21:29:09 +0100

    Added documentation for AFWebViewController

--- AFWebViewController/AFWebViewController.h
+++ AFWebViewController/AFWebViewController.h
@@ -10,14 +10,78 @@
 
 @interface AFWebViewController : UIViewController
 
+/**
+ *  Instantiate WebViewController with URL address string.
+ *
+ *  @param urlString String with URL to show in web view.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 + (instancetype)webViewControllerWithAddress:(NSString *)urlString;
+
+/**
+ *  Instantiate WebViewController with URL.
+ *
+ *  @param URL URL with address to show in web view.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 + (instancetype)webViewControllerWithURL:(NSURL *)URL;
+
+/**
+ *  Instantiate WebViewController with URL request.
+ *
+ *  @param request NSURLRequest to show in web view.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 + (instancetype)webViewControllerWithURLRequest:(NSURLRequest *)request;
+
+/**
+ *  Instantiate WebViewController with HTML string and base URL.
+ *
+ *  @param HTMLString HTML string to show in web view.
+ *  @param baseURL    Base URL containing local files like stylesheets etc.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 + (instancetype)webViewControllerWithHTMLString:(NSString *)HTMLString andBaseURL:(NSURL *)baseURL;
 
+/**
+ *  Instantiate WebViewController with URL address string.
+ *
+ *  @param urlString String with URL to show in web view.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 - (instancetype)initWithAddress:(NSString *)urlString;
+
+/**
+ *  Instantiate WebViewController with URL.
+ *
+ *  @param URL URL with address to show in web view.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 - (instancetype)initWithURL:(NSURL *)URL;
+
+/**
+ *  Instantiate WebViewController with URL request.
+ *
+ *  @param request NSURLRequest to show in web view.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 - (instancetype)initWithURLRequest:(NSURLRequest *)request NS_DESIGNATED_INITIALIZER;
+
+/**
+ *  Instantiate WebViewController with HTML string and base URL.
+ *
+ *  @param HTMLString HTML string to show in web view.
+ *  @param baseURL    Base URL containing local files like stylesheets etc.
+ *
+ *  @return Instance of `AFWebViewController`.
+ */
 - (instancetype)initWithHTMLString:(NSString *)HTMLString andBaseURL:(NSURL *)baseURL NS_DESIGNATED_INITIALIZER;
 
 @end
