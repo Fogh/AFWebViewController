6423f68abe7ea64d4cb6a5be06c42b0ae746b04d
Anders Fogh Eriksen <anders@afogh.dk>
2014-12-25 12:58:45 +0100

    Changed to use @import

--- Demo/ViewController.h
+++ Demo/ViewController.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Fogh Development. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface ViewController : UIViewController
 @end
--- Demo/main.m
+++ Demo/main.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Fogh Development. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
