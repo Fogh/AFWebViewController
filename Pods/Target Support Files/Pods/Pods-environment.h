fb9a0c6f6c7d17e8eff98e720f286606cacc6d2e
Anders Fogh Eriksen <anders@afogh.dk>
2015-07-08 21:55:16 +0200

    Update pods

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -16,5 +16,5 @@
 #define COCOAPODS_POD_AVAILABLE_TUSafariActivity
 #define COCOAPODS_VERSION_MAJOR_TUSafariActivity 1
 #define COCOAPODS_VERSION_MINOR_TUSafariActivity 0
-#define COCOAPODS_VERSION_PATCH_TUSafariActivity 2
+#define COCOAPODS_VERSION_PATCH_TUSafariActivity 4
 
